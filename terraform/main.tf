0b67a238bcdcafabf02199607f3709261d1c4042
Pruthvi Matta <pruthvimatta@MacBookPro.lan>
2024-10-13 23:01:48 -0500

    updated terraform module

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,9 +12,9 @@ module "vpc" {
   azs             = ["us-east-1a", "us-east-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
-  enable_nat_gateway = true
-  enable_dns_hostnames = true
 }
+  #enable_nat_gateway = true
+  #enable_dns_hostnames = true
 
 # IAM Role for EKS
 resource "aws_iam_role" "eks_cluster_role" {
