e172b998896e0d4fc477658c03f5cc46d0ad74e5
Pruthvi Matta <pruthvimatta@MacBookPro.lan>
2024-10-17 22:43:27 -0500

    updated region

--- terraform/main.tf
+++ terraform/main.tf
@@ -201,7 +201,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket"
     key            = "devopslabs/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "us-west-2"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
